95e020236aed423c40b8ebca1f60b9f362676e27
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-16 09:28:35 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,13 +2,13 @@
 
 echo "Rachi::Hey Pavi,How are you?"
 echo "Pavi::Hello Rachi, I am fine.How are you doing?"
-echo "Rachi::I am fine too. When i get a job,I want to meet you.
-echo "Pavi::Really? That will be very nice.
-echo "Rachi::Yes,I will give you a party.
-echo "Pavi::Wow,thank you.
-echo "Rachi::We can go to a restaurant and enjoy.
-echo "Pavi::Yes,we can eat good food and talk a lot.
-echo "Rachi::We can also take photos together.
-echo "Pavi::That will be a happy day.
-echo "Rachi::First i will get a job,then i will meet you.
-echo "Pavi::Yes,i am waiting for that day..
+echo "Rachi::I am fine too. When i get a job,I want to meet you."
+echo "Pavi::Really? That will be very nice."
+echo "Rachi::Yes,I will give you a party."
+echo "Pavi::Wow,thank you."
+echo "Rachi::We can go to a restaurant and enjoy."
+echo "Pavi::Yes,we can eat good food and talk a lot."
+echo "Rachi::We can also take photos together."
+echo "Pavi::That will be a happy day."
+echo "Rachi::First i will get a job,then i will meet you."
+echo "Pavi::Yes,i am waiting for that day.."
