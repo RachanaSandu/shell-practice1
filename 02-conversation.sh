4f94e678c3865285b40baf1487138e4f28e0e66d
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-18 22:08:49 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,6 +2,7 @@
 #here we use Rachi and Pavi words multiple times
 # here the disadvantage is if i don't use variables,while i changing every single same word.there is a chance to do mistake.
 # even one word spelling mistake will happen.that's why we will use Variables..
+# in first line only Shebang and next any line with '#' it takes as a Comment..
 echo "Rachi::Hey Pavi,How are you?"
 echo "Pavi::Hello Rachi, I am fine.How are you doing?"
 echo "Rachi::I am fine too. When i get a job,I want to meet you."
