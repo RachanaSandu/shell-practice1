5498de5fe4aa0497f3825fc668b9d0c427dcbc86
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-19 01:07:03 +0530

    data types

---
+++ 08-data types.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+NUMBER1=100
+NUMBER2=200
+
+TIMESTAMP=$(date)
+echo "script executed at:$TIMESTAMP"
+# TIMESTAMP is a variable.
+# if you want to know when the script was edited, you need to use 'date' command.
+# shell will execute 'date' command then stored the value in 'TIMESTAMP' variable.
