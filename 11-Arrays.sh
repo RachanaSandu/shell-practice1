27dd21c39c0e2c881729141c4daa99885135238c
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-19 02:03:02 +0530

    arrays

---
+++ 11-Arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DEVOPS=("Linux" "Shell scripting" "Ansible" "Terraform" "Docker" "Kubernetes" "Git" "Jenkins" "Python")
+
+echo "First Devops topic is: $MOVIES[0]"
+
+#Array means it will stores multiple values
+# in DEVOPS we will store devops topic names within double quotes and with space difference.
+#DEVOPS=("Linux" "Shell scripting" "Ansible" "Terraform" "Docker" "Kubernetes" "Git" "Jenkins" "Python") 
+# in above syntax, The size of this syntax is 9, and The index possition starts from 0 to 8.like first devops topic is Linux.
