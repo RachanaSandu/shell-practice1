306d919e01749a162b0edf68a3ab0e70db19af88
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-18 23:41:09 +0530

    variable

--- 06-variable3.sh
+++ 06-variable3.sh
@@ -1,6 +1,7 @@
-#!bin/bash
+#!/bin/bash
 
 echo "enter your pin number ::"
-read PIN #here PIN is a variable.
+read PIN 
+#here PIN is a variable, here we use 'read' command..
 echo "your number is : $PIN"
 # in this script while executing pin number will shown, this script only for understanding..
---
+++ 07-variable4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "enter your pin number ::"
+read -s PIN 
+echo "your number is : $PIN"
+#here PIN is a variable, here we use 'read' command..
+# in this script while executing the pin number will not shown.
+# because we use '-s' in 'read' command for hiding the pin number.
