7d9b43921023debbd5ae53bbe7a2956c2f919d35
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-19 01:31:29 +0530

    datatypes

---
+++ 10-data-types2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#!/bin/bash
+
+NUMBER1=Rachana
+NUMBER2=200
+
+TIMESTAMP=$(date)
+
+echo "script executed at : $TIMESTAMP"
+
+SUM=$(($NUMBER1+$NUMBER2))
+
+echo "SUM of $NUMBER1 and $NUMBER2 is : $SUM"
+
+# TIMESTAMP is a variable.
+# if you want to know when the script was edited, you need to use 'date' command.
+# shell will execute 'date' command then stored the value in 'TIMESTAMP' variable.
+
+# i didn't give int=100, but Shell runs automatically no need to mention.
+# here we use two braces, in that braces '($NUMBER1+$NUMBER2)' will adding values. 
+# '$()' will store that added value. and next that $(($NUMBER1+$NUMBER2)) value will store in 'SUM' Variable.
+
+# if i give 'NUMBER1=Rachana' it consider this 'Rachana' value as String, it takes String as 'Zero' value.
+
