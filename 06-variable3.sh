cdfac88ba9d0c607901507b541a17fdc677d001f
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-18 23:08:06 +0530

    variable3

---
+++ 06-variable3.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo "enter your pin number ::"
+read PIN #here PIN is a variable.
+echo "your number is : $PIN"
+# in this script while executing pin number will shown, this script only for understanding..
