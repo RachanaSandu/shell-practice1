0d9b268522ce69e5e38caec5b2c7197a143876ca
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-21 11:38:12 +0530

    arrays

--- 11-Arrays.sh
+++ 11-Arrays.sh
@@ -8,3 +8,4 @@ echo "First Devops topic is: $DEVOPS[0]"
 # in DEVOPS we will store devops topic names within double quotes and with space difference.
 #DEVOPS=("Linux" "Shell scripting" "Ansible" "Terraform" "Docker" "Kubernetes" "Git" "Jenkins" "Python") 
 # in above syntax, The size of this syntax is 9, and The index possition starts from 0 to 8.like first devops topic is Linux.
+# here,the output comes like 
