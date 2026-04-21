09cbd9ae6c4ff0f162ea78226225f81371aab52c
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-21 12:37:36 +0530

    conversation

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,6 +3,7 @@
 # here the disadvantage is if i don't use variables,while i changing every single same word.there is a chance to do mistake.
 # even one word spelling mistake will happen.that's why we will use Variables..
 # in first line only Shebang and next any line with '#' it takes as a Comment..
+# we can Run the Script using sh<script name> or bash<script name> or ./<script name> , for ./<script name> only has with execution permission.
 echo "Rachi::Hey Pavi,How are you?"
 echo "Pavi::Hello Rachi, I am fine.How are you doing?"
 echo "Rachi::I am fine too. When i get a job,I want to meet you."
