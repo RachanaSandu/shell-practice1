bb00210d6352479a005dcf1b3767fe9ce6657992
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-26 10:09:11 +0530

    special variables

--- 14-special-variables.sh
+++ 14-special-variables.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo"we no need to define the variable, shell will provide to us :$1"
-echo"if you want all variables passed to script : $@"
-echo"number of variables passed : $echo"
-echo"present working directory : $PWD"
-echo"home directory of the user who is running the script : $HOME"
-echo"which user is running the script : $USER"
-echo"PID(process instance ID) of the last command running  in background : $!"
-echo"PID of the current script : $$"
+# we no need to define the variable, shell will provide to us :$1 
+# if you want all variables passed to script : $@ 
+# number of variables passed : $# 
+# present working directory : $PWD 
+# home directory of the user who is running the script : $HOME 
+# which user is running the script : $USER 
+# PID(process instance ID) of the last command running  in background : $! 
+# PID of the current script : $$ 
 
 
+echo"All variables passed to the script:$@"
+echo"Number of variables:$#"
