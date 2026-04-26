1cd2956ac44a14d92bfb49eaec539adbafcca316
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-26 12:09:42 +0530

    conditions

---
+++ 16-conditions1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# SHELL OPERATORS
+# -gt means Greater than
+# -lt means Less than
+# -eq means Equal 
+# -ne means Not equal
+
+NUMBER=$1
+if [ $NUMBER -lt 10 ]
+then
+  echo "Given number $NUMBER is less than 10"
+else 
+  echo "Given number $NUMBER is not less than 10"
+fi
