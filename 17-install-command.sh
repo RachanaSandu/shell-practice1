41816165d986619aaaee1922bf40e02a34f3998a
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-28 21:08:12 +0530

    install -command

--- 17-install-command.sh
+++ 17-install-command.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-USERID=$(id -u)
+# check the user information $ id
+# check root user information $ sudo id
+# check only user id $ id -u
+ 
+USERID=$(id -u) 
 
 if [ $USERID -ne 0 ]
 then 
@@ -8,16 +12,18 @@ then
   exit 1 #give otherthan 0 upto 127
 else  
   echo "you are running this script with root access"
-  fi
+fi
 
-  dnf list installed mysql
+  dnf list installed mysql  
 
-  if [ $? -ne 0 ]
+# check already installed or not. if installed $? is 0, then
+# if not installed $? is not 0. expression is true 
+# we can write script with if(condition) within if(condition) again
+if [ $? -ne 0 ] # if it didn't installed 
 then
-  echo "Mysql is not installed...going to install it"
- dnf install mysql -y
-
-  if [ $? -eq 0 ]
+  echo "Mysql is not installed...going to install it"  # then do install 
+ dnf install mysql -y  # run this script with root access using sudo command like sudo su 17-install-command.sh
+  if [ $? -eq 0 ]  # checking installed or not
   then 
     echo "Installing MYSQL is ...SUCCESS"
 else
@@ -25,6 +31,6 @@ else
    exit 1
    fi
 else
-  echo "Mysql is already installed...Nothing to do"
+  echo "Mysql is already installed...Nothing to do"  # else nothing to install
 fi
   
