aa85ec2c3e41b5ec2d6f64b3930b5c70685a80c9
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-30 20:21:43 +0530

    colors

--- 20-colors.sh
+++ 20-colors.sh
@@ -1,4 +1,6 @@
 #!\bin\bash
 
-echo -e "\e[34m DEVOPS \e[0m"
-echo "aws"
+# 31 -> red   32 -> green   33 -> yellow  34 ->blue  35 -> megenda  36 -> gyan  37 and 0 -> white                                            
+# bold/bright -> 1   underline -> 4  italic -> 3
+echo -e "\e[34m DEVOPS \e[0m" # DEVOPS shows in blue color
+echo -e "\e1m \e[31m D \e[32m E \e[33m V \e[34m O \e[35m P \e[36m S"
