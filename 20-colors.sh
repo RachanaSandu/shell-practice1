d19d2443f1e360c2345b98015b2d655fd25db30b
RachanaSandu <rachanasandhu3@gmail.com>
2026-04-30 20:29:42 +0530

    colors

--- 20-colors.sh
+++ 20-colors.sh
@@ -3,4 +3,7 @@
 # 31 -> red   32 -> green   33 -> yellow  34 ->blue  35 -> megenda  36 -> gyan  37 and 0 -> white                                            
 # bold/bright -> 1   underline -> 4  italic -> 3
 echo -e "\e[34m DEVOPS \e[0m" # DEVOPS shows in blue color
-echo -e "\e[1m \e[31m D \e[32m E \e[33m V \e[34m O \e[35m P \e[36m S"
+echo -e "\e[37m Devops"
+echo -e "\e[1m \e[31m d \e[32m e \e[33m v \e[34m o \e[35m p \e[36m s"
+echo -e "\e[4m \e[31m d \e[32m e \e[33m v \e[34m o \e[35m p \e[36m s"
+echo -e "\e[3m \e[31m d \e[32m e \e[33m v \e[34m o \e[35m p \e[36m s"
